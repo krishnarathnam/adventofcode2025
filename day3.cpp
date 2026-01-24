d1a24eadc4af030d145cb8b12f57ff6648ca045b
Krishna Rathnam <kr130205@gmail.com>
2026-01-25 01:13:34 +0530

    day 3 30 min

---
+++ day3.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+  std::ifstream file("./input/day3.txt");
+  std::string line;
+  std::vector<int> res;
+  int n1, n2 = INT_MIN;
+  int sum = 0;
+  while (std::getline(file, line)) {
+    std::cout << line << std::endl;
+    int largest = INT_MIN;
+    int num = 0;
+    for (int i = 0; i < line.size() - 1; i++) {
+      for (int j = i + 1; j < line.size(); j++) {
+        num = (line.at(i) - '0') * 10 + (line.at(j) - '0');
+        if (num > largest) {
+          largest = num;
+        }
+      }
+    }
+    res.push_back(largest);
+  }
+  for (auto x : res) {
+    sum += x;
+  }
+
+  std::cout << sum << std::endl;
+}
