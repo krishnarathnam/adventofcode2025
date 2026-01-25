86ca9623fe50d697e302378e5e32c7d7b5045adb
Krishna Rathnam <kr130205@gmail.com>
2026-01-25 14:37:51 +0530

    day1 part 2 15min

--- day1part2.cpp
+++ day1part2.cpp
@@ -1,10 +1,11 @@
+
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
 
 int main() {
-  std::ifstream file("./input/day1part2_demo.txt");
+  std::ifstream file("./input/day1part2.txt");
 
   if (!file) {
     std::cerr << "Error opening file" << '\n';
@@ -14,6 +15,7 @@ int main() {
   std::string line;
   int count = 0;
   int sum = 50;
+
   while (std::getline(file, line)) {
     char c = line[0];
     int d = 0;
@@ -21,10 +23,26 @@ int main() {
       d = d * 10 + (line[t] - '0');
     }
 
-    sum += (c == 'R') ? d : -d;
+    if (c == 'R') {
+      for (int i = 0; i < d; i++) {
+        sum++;
+        if (sum == 100)
+          sum = 0;
+        if (sum == 0)
+          count++;
+      }
+    } else if (c == 'L') {
+      for (int i = 0; i < d; i++) {
+        sum--;
+        if (sum == -1)
+          sum = 99;
+        if (sum == 0)
+          count++;
+      }
+    }
 
     if ((sum % 100 == 0)) {
-      count++;
+      // count++;
     }
     std::cout << line << " Direction: " << c << " Degree: " << d
               << " sum: " << sum << '\n';
